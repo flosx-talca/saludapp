3ed354eb082b28bca69da7330b38fe09d6bdebe4
flosx-talca <orlandorozasi@gmail.com>
2025-06-12 22:16:01 -0400

    si

---
+++ src/main/java/com/equipo/saludoTest/App.java
@@ -0,0 +1,14 @@
+package com.equipo.saludoTest;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class App {
+
+    public static String saludar(String nombre){
+        return "Hola " + nombre + "!!";
+    }
+    public static void main(String[] args) {
+        System.out.println(saludar("DEVOPS"));
+    }
+}
---
+++ src/test/java/com/equipo/saludoTest/AppTest.java
@@ -0,0 +1,12 @@
+package com.equipo.saludoTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testSaludo(){
+        assertEquals("Hola DEVOPS!!", App.saludar("DEVOPS"));
+    }
+}
