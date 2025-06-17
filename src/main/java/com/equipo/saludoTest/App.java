041b67cd3387e33c0ddb981e9821e59cb6e02f96
flosx-talca <orlandorozasi@gmail.com>
2025-06-16 21:47:17 -0400

    hola mundo11

--- src/main/java/com/equipo/saludoTest/App.java
+++ src/main/java/com/equipo/saludoTest/App.java
@@ -8,6 +8,10 @@ public class App {
     public static String saludar(String nombre){
        
         return "Hola " + nombre + "!!";
+
+
+
+
     }
     public static void main(String[] args) {
         System.out.println(saludar("DEVOPS"));
