3b715b2a8e379efe6727eaf678ee4bb0c4ed5395
flosx-talca <orlandorozasi@gmail.com>
2025-06-16 23:05:15 -0400

    prueba

--- src/main/java/com/equipo/saludotest/App.java
+++ src/main/java/com/equipo/saludotest/App.java
@@ -1,11 +1,15 @@
 package com.equipo.saludotest;
 
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
+
     public static String saludar(String nombre){
+
         return "Hola " + nombre + "!!";
     }
     public static void main(String[] args) {
         System.out.println(saludar("DEVOPS"));
-        System.out.print("Hello and welcome!");
     }
 }
